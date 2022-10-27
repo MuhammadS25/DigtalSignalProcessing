92ba758b89daf74d86589a19f98942ab4f130269
Muhammad Sabry <mohammed20191700542@cis.asu.edu.eg>
2022-10-27 21:29:01 +0200

    Adding Second task files

---
+++ DSPComponents/Algorithms/DC_Component.cs
@@ -0,0 +1,20 @@
+﻿using DSPAlgorithms.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSPAlgorithms.Algorithms
+{
+    public class DC_Component: Algorithm
+    {
+        public Signal InputSignal { get; set; }
+        public Signal OutputSignal { get; set; }
+
+        public override void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DSPComponents/Algorithms/SinCos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using DSPAlgorithms.DataStructures;
+
+namespace DSPAlgorithms.Algorithms
+{
+    public class SinCos: Algorithm
+    {
+        public string type { get; set; }
+        public float A { get; set; }
+        public float PhaseShift { get; set; }
+        public float AnalogFrequency { get; set; }
+        public float SamplingFrequency { get; set; }
+        public List<float> samples { get; set; }
+        public override void Run()
+        {
+            
+            throw new NotImplementedException();
+        }
+    }
+}
