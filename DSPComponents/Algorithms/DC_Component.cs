19e1acbdb50ed4989a3e09a76ebe642138286657
Muhammad Sabry <mohammed20191700542@cis.asu.edu.eg>
2022-10-28 00:15:17 +0200

    DC Component Solved

--- DSPComponents/Algorithms/DC_Component.cs
+++ DSPComponents/Algorithms/DC_Component.cs
@@ -7,14 +7,22 @@ using System.Threading.Tasks;
 
 namespace DSPAlgorithms.Algorithms
 {
-    public class DC_Component: Algorithm
+    public class DC_Component : Algorithm
     {
         public Signal InputSignal { get; set; }
         public Signal OutputSignal { get; set; }
 
         public override void Run()
         {
-            throw new NotImplementedException();
+            float tot = 0;
+            for (int i = 0; i < InputSignal.Samples.Count; i++)
+                tot += InputSignal.Samples[i];
+            tot /= InputSignal.Samples.Count;
+            OutputSignal = new Signal(new List<float>(), false);
+            for (int i = 0; i < InputSignal.Samples.Count; i++)
+            {
+                OutputSignal.Samples.Add(InputSignal.Samples[i] - tot);
+            }
         }
     }
 }
