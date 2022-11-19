2133b4fb410f07b961c8261fb943e43098e144e2
Mazen Khaled <93441375+3bshafy@users.noreply.github.com>
2022-11-19 18:49:50 +0200

    added required .cs files

---
+++ DSPComponents/Algorithms/AccumulationSum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSPAlgorithms.DataStructures;
+
+
+namespace DSPAlgorithms.Algorithms
+{
+    public class AccumulationSum : Algorithm
+    {
+        public Signal InputSignal { get; set; }
+        public Signal OutputSignal { get; set; }
+
+        public override void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DSPComponents/Algorithms/Derivatives.cs
@@ -0,0 +1,23 @@
+﻿using DSPAlgorithms.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSPAlgorithms.Algorithms
+{
+    public class Derivatives: Algorithm
+    {
+        public Signal InputSignal { get; set; }
+        public Signal FirstDerivative { get; set; }
+        public Signal SecondDerivative { get; set; }
+
+        public override void Run()
+        {
+
+           throw new NotImplementedException();
+        }
+    }
+}
