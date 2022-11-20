977c9706002d72d4b0f3d118c5317a72ce758ef3
Muhammad Sabry <mohammed20191700542@cis.asu.edu.eg>
2022-11-20 02:12:51 +0200

    Derivatives is Solved

--- DSPComponents/Algorithms/Derivatives.cs
+++ DSPComponents/Algorithms/Derivatives.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace DSPAlgorithms.Algorithms
 {
-    public class Derivatives: Algorithm
+    public class Derivatives : Algorithm
     {
         public Signal InputSignal { get; set; }
         public Signal FirstDerivative { get; set; }
@@ -16,8 +16,27 @@ namespace DSPAlgorithms.Algorithms
 
         public override void Run()
         {
+            FirstDerivative = new Signal(new List<float>(), false);
+            SecondDerivative = new Signal(new List<float>(), false);
+            FirstDerivative.SamplesIndices = new List<int>();
+            FirstDerivative.Samples = new List<float>();
+            SecondDerivative.SamplesIndices = new List<int>();
+            SecondDerivative.Samples = new List<float>();
+            for (int i = 0; i < InputSignal.Samples.Count - 1; i++)
+            {
+                FirstDerivative.Samples.Add
+                    (InputSignal.Samples[i + 1] - InputSignal.Samples[i]);
 
-           throw new NotImplementedException();
+                FirstDerivative.SamplesIndices.Add(i);
+            }
+            for (int i = 0; i < InputSignal.Samples.Count - 2; i++)
+            {
+                SecondDerivative.Samples.Add
+                    (InputSignal.Samples[i + 2] -
+                    2 * InputSignal.Samples[i + 1] + InputSignal.Samples[i]);
+
+                SecondDerivative.SamplesIndices.Add(i);
+            }
         }
     }
 }
