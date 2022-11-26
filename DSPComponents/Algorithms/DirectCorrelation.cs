74554611a2e52dfe37d3e669c6a3fc513402cb6e
Muhammad Sabry <mohammed20191700542@cis.asu.edu.eg>
2022-11-26 20:35:43 +0200

    Adding Cross Correlation Periodic Signals

--- DSPComponents/Algorithms/DirectCorrelation.cs
+++ DSPComponents/Algorithms/DirectCorrelation.cs
@@ -20,8 +20,7 @@ namespace DSPAlgorithms.Algorithms
             for (int i = 0; i < S.Samples.Count; i++)
                 res += S.Samples[i] * S.Samples[i];
 
-            float Norm = (float)1 / S.Samples.Count * res; //sqrt of the squared value
-            return Norm;
+            return res;
         }
         public override void Run()
         {
@@ -35,6 +34,7 @@ namespace DSPAlgorithms.Algorithms
                 if (!InputSignal1.Periodic)
                 {
                     float norm = Normalization(InputSignal1);
+                    norm = (float)1 / InputSignal1.Samples.Count * (float)Math.Sqrt(norm * norm);
                     int shifts = 0;
                     for (int i = 0; i < InputSignal1.Samples.Count; i++)
                     {
@@ -51,6 +51,7 @@ namespace DSPAlgorithms.Algorithms
                 else
                 {
                     float norm = Normalization(InputSignal1);
+                    norm = (float)1 / InputSignal1.Samples.Count * (float)Math.Sqrt(norm * norm);
                     Signal input11 = new Signal(new List<float>(), new List<int>(), InputSignal1.Periodic);
                     for (int i = 0; i < InputSignal1.Samples.Count; i++)
                         input11.Samples.Add(InputSignal1.Samples[i]);
@@ -67,6 +68,39 @@ namespace DSPAlgorithms.Algorithms
                         float Shifted = input11.Samples[0];
                         input11.Samples.Remove(input11.Samples[0]);
                         input11.Samples.Add(Shifted);
+                    }
+                }
+            }
+            else
+            {
+                if (InputSignal1.Periodic)
+                {
+                    if (InputSignal1.Samples.Count != InputSignal2.Samples.Count)
+                    {
+                        for (int i = 0; i < InputSignal2.Samples.Count - 1; i++)
+                            InputSignal1.Samples.Add(0);
+                        for (int i = 0; i < InputSignal1.Samples.Count - 1; i++)
+                            InputSignal2.Samples.Add(0);
+                    }
+
+                    float norm1 = Normalization(InputSignal1);
+                    float norm2 = Normalization(InputSignal2);
+                    float norm = (float)1 / InputSignal1.Samples.Count * (float)Math.Sqrt(norm1 * norm2);
+
+                    for (int i = 0; i < InputSignal1.Samples.Count; i++)
+                    {
+                        float corr = 0;
+                        for (int j = 0; j < InputSignal1.Samples.Count; j++)
+                            corr += (InputSignal1.Samples[j] * InputSignal2.Samples[j]);
+
+                        corr /= InputSignal1.Samples.Count;
+
+                        OutputNonNormalizedCorrelation.Add(corr);
+                        OutputNormalizedCorrelation.Add(corr / norm);
+
+                        float Shifted = InputSignal2.Samples[0];
+                        InputSignal2.Samples.Remove(InputSignal2.Samples[0]);
+                        InputSignal2.Samples.Add(Shifted);
                     }
                 }
             }
