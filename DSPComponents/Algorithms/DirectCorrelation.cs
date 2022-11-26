ec22c1d3a758bed4780f256deae54256dcf108df
Mazen Khaled <93441375+3bshafy@users.noreply.github.com>
2022-11-26 12:22:54 +0200

    Handeled AutoCorrelation case
    cross correlation isnt done yet but autocorrelation is done

--- DSPComponents/Algorithms/DirectCorrelation.cs
+++ DSPComponents/Algorithms/DirectCorrelation.cs
@@ -16,6 +16,59 @@ namespace DSPAlgorithms.Algorithms
 
         public override void Run()
         {
+            OutputNonNormalizedCorrelation = new List<float>();
+            OutputNormalizedCorrelation = new List<float>();
+
+            if (InputSignal2 == null)
+            {
+                InputSignal2 = new Signal(InputSignal1.Samples, InputSignal1.Periodic);
+                double Sum1 = 0 , Sum2 = 0 , SumTot = 0;
+
+                for(int i=0; i<InputSignal1.Samples.Count; i++)
+                {
+                    Sum1 += Math.Pow(InputSignal1.Samples[i], 2);
+                }
+                Sum2 = Sum1;
+                SumTot = Sum1 * Sum2;
+                double NrmlFctr = (1 / ((1 / InputSignal1.Samples.Count) * Math.Sqrt(SumTot)));
+               
+                for(int i=0; i<InputSignal1.Samples.Count; i++)
+                {
+                    double nonNorm = 0 , Norm = 0;
+                    for(int j=0; j<InputSignal1.Samples.Count; j++)
+                    {
+                        nonNorm += InputSignal1.Samples[j] * InputSignal2.Samples[j];
+                    }
+                    Norm = NrmlFctr * nonNorm;
+                    OutputNonNormalizedCorrelation[i] = (float)nonNorm;
+                    OutputNormalizedCorrelation[i] = (float)Norm;
+                    if (InputSignal2.Periodic)
+                    {
+                        float first = InputSignal2.Samples[0];
+                        InputSignal2.Samples.RemoveAt(0);
+                        InputSignal2.Samples.Add(first);
+                    }
+                    else
+                    {
+                        InputSignal2.Samples.RemoveAt(0);
+                        InputSignal2.Samples.Add(0);
+                    }
+                }
+            }
+            else
+            {
+                double Sum1 = 0, Sum2 = 0, SumTot = 0;
+                for (int i = 0; i < InputSignal1.Samples.Count; i++)
+                {
+                    Sum1 += Math.Pow(InputSignal1.Samples[i], 2);
+                }
+                for (int i = 0; i < InputSignal2.Samples.Count; i++)
+                {
+                    Sum2 += Math.Pow(InputSignal2.Samples[i], 2);
+                }
+                SumTot = Sum1 * Sum2;
+                double NrmlFctr = (1 / ((1 / InputSignal1.Samples.Count) * Math.Sqrt(SumTot)));
+            }
             throw new NotImplementedException();
         }
     }
