a15f1a66f2575033462f38a7f2a793400ce2ce68
Mazen Khaled <93441375+3bshafy@users.noreply.github.com>
2022-11-26 10:53:17 +0200

    added task 5 files

---
+++ DSPComponents/Algorithms/TimeDelay.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSPAlgorithms.DataStructures;
+
+namespace DSPAlgorithms.Algorithms
+{
+    public class TimeDelay:Algorithm
+    {
+        public Signal InputSignal1 { get; set; }
+        public Signal InputSignal2 { get; set; }
+        public float InputSamplingPeriod { get; set; }
+        public float OutputTimeDelay { get; set; }
+
+        public override void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
