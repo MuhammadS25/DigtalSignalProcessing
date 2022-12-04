751a7471ee151426dd948c9f921bfcc71f0d5cff
Muhammad Sabry <mohammed20191700542@cis.asu.edu.eg>
2022-12-04 05:35:58 +0200

    Adding DCT solution

--- DSPComponents/Algorithms/DCT.cs
+++ DSPComponents/Algorithms/DCT.cs
@@ -7,14 +7,27 @@ using System.Threading.Tasks;
 
 namespace DSPAlgorithms.Algorithms
 {
-    public class DCT: Algorithm
+    public class DCT : Algorithm
     {
         public Signal InputSignal { get; set; }
         public Signal OutputSignal { get; set; }
 
         public override void Run()
         {
-            throw new NotImplementedException();
+            List<float> L = new List<float>();
+            float N = InputSignal.Samples.Count;
+            for (int k = 0; k < N; k++)
+            {
+                float value = 0;
+                for (int n = 0; n < N; n++)
+                {
+                    value += (float)(InputSignal.Samples[n] * Math.Cos((Math.PI / (4 * N)) * (2 * n - 1) * (2 * k - 1)));
+                }
+                value *= (float)Math.Sqrt(2 / N);
+                L.Add(value);
+            }
+            OutputSignal = new Signal(L, false);
+
         }
     }
 }
