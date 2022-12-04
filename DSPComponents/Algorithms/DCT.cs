6025f6ad40cee78476d13383a84ec0918cb3de75
Muhammad Sabry <mohammed20191700542@cis.asu.edu.eg>
2022-12-04 03:06:48 +0200

    Adding DCT Files

---
+++ DSPComponents/Algorithms/DCT.cs
@@ -0,0 +1,20 @@
+﻿using DSPAlgorithms.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSPAlgorithms.Algorithms
+{
+    public class DCT: Algorithm
+    {
+        public Signal InputSignal { get; set; }
+        public Signal OutputSignal { get; set; }
+
+        public override void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
